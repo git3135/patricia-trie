026c3933b724d47370199f12cf425d045f337e43
rkapsi <rkapsi@986d498a-bda2-11dd-b57b-7192f494c34b>
2008-11-29 02:48:00 +0000

    Adding missing Class

---
+++ src/main/java/org/ardverk/collection/AbstractKeyAnalyzer.java
@@ -0,0 +1,14 @@
+package org.ardverk.collection;
+
+public abstract class AbstractKeyAnalyzer<K> implements KeyAnalyzer<K> {
+    
+    private static final long serialVersionUID = 384115794654304353L;
+
+    public static final int[] createIntBitMask(int bitCount) {
+        int[] bits = new int[bitCount];
+        for (int i = 0; i < bitCount; i++) {
+            bits[i] = 1 << (bitCount - i - 1);
+        }
+        return bits;
+    }
+}
