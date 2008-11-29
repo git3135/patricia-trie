5ee9f1525caf72c7caf1b6f7ae96c8f896d5bef7
rkapsi <rkapsi@986d498a-bda2-11dd-b57b-7192f494c34b>
2008-11-29 01:56:23 +0000

    Copyright Note

--- src/main/java/org/ardverk/collection/PatriciaTrie.java
+++ src/main/java/org/ardverk/collection/PatriciaTrie.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2008 Roger Kapsi, Sam Berlin
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package org.ardverk.collection;
 
 import java.io.Serializable;
@@ -102,6 +118,9 @@ public class PatriciaTrie<K, V> extends AbstractMap<K, V>
         this.keyAnalyzer = keyAnalyzer;
     }
     
+    /**
+     * 
+     */
     public PatriciaTrie(KeyAnalyzer<? super K> keyAnalyzer, 
             Map<? extends K, ? extends V> m) {
         if (keyAnalyzer == null) {
@@ -147,14 +166,6 @@ public class PatriciaTrie<K, V> extends AbstractMap<K, V>
         
         size = 0;
         incrementModCount();
-    }
-    
-    /**
-     * Returns true if the {@link Trie} is empty
-     */
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
     }
     
     /**
@@ -657,6 +668,7 @@ public class PatriciaTrie<K, V> extends AbstractMap<K, V>
     }
     
     /** Returns true if this Trie contains the specified value. */
+    @Override
     public boolean containsValue(Object o) {
         for (V v : values()) {
             if (valEquals(v, o)) {
@@ -1122,6 +1134,13 @@ public class PatriciaTrie<K, V> extends AbstractMap<K, V>
      */
     private int bitIndex(K key, K foundKey) {
         return keyAnalyzer.bitIndex(key, 0, length(key), foundKey, 0, length(foundKey));
+    }
+    
+    /**
+     * An utility method to compare values.
+     */
+    private static boolean valEquals(Object o1, Object o2) {
+        return (o1 == null ? o2 == null : o1.equals(o2));
     }
     
     /** The actual Trie nodes. */
@@ -1603,11 +1622,6 @@ public class PatriciaTrie<K, V> extends AbstractMap<K, V>
         return values;
     }
     
-    /** Test two values for equality.  Works with null values. */
-    private static boolean valEquals(Object o1, Object o2) {
-        return (o1==null ? o2==null : o1.equals(o2));
-    }
-
     private class Values extends AbstractCollection<V> {
         
         @Override
