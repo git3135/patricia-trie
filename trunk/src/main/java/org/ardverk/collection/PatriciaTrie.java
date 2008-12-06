046436ffd262638aee1dab9f74cd338e0772cb23
rkapsi <rkapsi@986d498a-bda2-11dd-b57b-7192f494c34b>
2008-12-06 20:42:44 +0000

    Renaming a class and moving more code to it

--- trunk/src/main/java/org/ardverk/collection/PatriciaTrie.java
+++ trunk/src/main/java/org/ardverk/collection/PatriciaTrie.java
@@ -73,7 +73,7 @@ import org.ardverk.collection.Cursor.Decision;
  * @author Roger Kapsi
  * @author Sam Berlin
  */
-public class PatriciaTrie<K, V> extends AbstractPatriciaTrie<K, V> {
+public class PatriciaTrie<K, V> extends PatriciaTrieBase<K, V> {
     
     private static final long serialVersionUID = 6473154301823684262L;
 
@@ -1342,144 +1342,7 @@ public class PatriciaTrie<K, V> extends AbstractPatriciaTrie<K, V> {
         return entry;
     }
     
-    /**
-     * A {@link Reference} allows us to return something through a Method's 
-     * argument list. An alternative would be to an Array with a length of 
-     * one (1) but that leads to compiler warnings. Computationally and memory
-     * wise there's no difference (except for the need to load the 
-     * {@link Reference} Class but that happens only once).
-     */
-    private static class Reference<E> {
-        
-        private E item;
-        
-        public void set(E item) {
-            this.item = item;
-        }
-        
-        public E get() {
-            return item;
-        }
-    }
     
-    /**
-     *  A {@link Trie} is a set of {@link TrieEntry} nodes
-     */
-    private static class TrieEntry<K,V> extends BasicEntry<K, V> {
-        
-        private static final long serialVersionUID = 4596023148184140013L;
-        
-        /** The index this entry is comparing. */
-        private int bitIndex;
-        
-        /** The parent of this entry. */
-        private TrieEntry<K,V> parent;
-        
-        /** The left child of this entry. */
-        private TrieEntry<K,V> left;
-        
-        /** The right child of this entry. */
-        private TrieEntry<K,V> right;
-        
-        /** The entry who uplinks to this entry. */ 
-        private TrieEntry<K,V> predecessor;
-        
-        private TrieEntry(K key, V value, int bitIndex) {
-            super(key, value);
-            
-            this.bitIndex = bitIndex;
-            
-            this.parent = null;
-            this.left = this;
-            this.right = null;
-            this.predecessor = this;
-        }
-        
-        /**
-         * Whether or not the entry is storing a key.
-         * Only the root can potentially be empty, all other
-         * nodes must have a key.
-         */
-        public boolean isEmpty() {
-            return key == null;
-        }
-        
-        /** 
-         * Neither the left nor right child is a loopback 
-         */
-        private boolean isInternalNode() {
-            return left != this && right != this;
-        }
-        
-        /** 
-         * Either the left or right child is a loopback 
-         */
-        private boolean isExternalNode() {
-            return !isInternalNode();
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public String toString() {
-            StringBuilder buffer = new StringBuilder();
-            
-            if (bitIndex == -1) {
-                buffer.append("RootEntry(");
-            } else {
-                buffer.append("Entry(");
-            }
-            
-            buffer.append("key=").append(getKey()).append(" [").append(bitIndex).append("], ");
-            buffer.append("value=").append(getValue()).append(", ");
-            //buffer.append("bitIndex=").append(bitIndex).append(", ");
-            
-            if (parent != null) {
-                if (parent.bitIndex == -1) {
-                    buffer.append("parent=").append("ROOT");
-                } else {
-                    buffer.append("parent=").append(parent.getKey()).append(" [").append(parent.bitIndex).append("]");
-                }
-            } else {
-                buffer.append("parent=").append("null");
-            }
-            buffer.append(", ");
-            
-            if (left != null) {
-                if (left.bitIndex == -1) {
-                    buffer.append("left=").append("ROOT");
-                } else {
-                    buffer.append("left=").append(left.getKey()).append(" [").append(left.bitIndex).append("]");
-                }
-            } else {
-                buffer.append("left=").append("null");
-            }
-            buffer.append(", ");
-            
-            if (right != null) {
-                if (right.bitIndex == -1) {
-                    buffer.append("right=").append("ROOT");
-                } else {
-                    buffer.append("right=").append(right.getKey()).append(" [").append(right.bitIndex).append("]");
-                }
-            } else {
-                buffer.append("right=").append("null");
-            }
-            buffer.append(", ");
-            
-            if (predecessor != null) {
-                if(predecessor.bitIndex == -1) {
-                    buffer.append("predecessor=").append("ROOT");
-                } else {
-                    buffer.append("predecessor=").append(predecessor.getKey()).append(" [").append(predecessor.bitIndex).append("]");
-                }
-            }
-            
-            buffer.append(")");
-            return buffer.toString();
-        }
-    }
     
     /**
      * This is a entry set view of the {@link Trie} as returned 
