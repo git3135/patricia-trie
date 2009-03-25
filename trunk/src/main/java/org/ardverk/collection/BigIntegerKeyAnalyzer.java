99acac58b3b2142067361a45a9e9ced2db9af5fa
rkapsi <rkapsi@986d498a-bda2-11dd-b57b-7192f494c34b>
2009-03-25 03:23:09 +0000

    Adding code for BigInteger but it's not tested yet

--- trunk/src/main/java/org/ardverk/collection/BigIntegerKeyAnalyzer.java
+++ trunk/src/main/java/org/ardverk/collection/BigIntegerKeyAnalyzer.java
@@ -23,9 +23,128 @@ import java.math.BigInteger;
  * 
  * NOTE: THIS DOES NOT WORK YET!
  */
-class BigIntegerKeyAnalyzer { //implements KeyAnalyzer<BigInteger> {
+class BigIntegerKeyAnalyzer extends AbstractKeyAnalyzer<BigInteger> {
     
     private static final long serialVersionUID = 7123669849156062477L;
+
+    /**
+     * The maximum length of a key in bits
+     */
+    private final int maxLengthInBits;
     
+    public BigIntegerKeyAnalyzer(int maxLengthInBits) {
+        if (maxLengthInBits < 0) {
+            throw new IllegalArgumentException(
+                    "maxLengthInBits=" + maxLengthInBits);
+        }
+        
+        this.maxLengthInBits = maxLengthInBits;
+    }
     
+    /**
+     * Returns the maximum length of a key in bits
+     */
+    public int getMaxLengthInBits() {
+        return maxLengthInBits;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int bitsPerElement() {
+        return 1;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int lengthInBits(BigInteger key) {
+        return (key != null ? key.bitCount() : 0);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isBitSet(BigInteger key, int bitIndex, int lengthInBits) {
+        if (key == null) {     
+            return false;
+        }
+        
+        int prefix = maxLengthInBits - lengthInBits;
+        int keyBitIndex = bitIndex - prefix;
+        
+        if (keyBitIndex >= lengthInBits || keyBitIndex < 0) {
+            return false;
+        }
+        
+        return key.testBit(keyBitIndex);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int bitIndex(BigInteger key, int offsetInBits, int lengthInBits, 
+            BigInteger other, int otherOffsetInBits, int otherLengthInBits) {
+        
+        if (other == null) {
+            other = BigInteger.ZERO;
+        }
+        
+        boolean allNull = true;
+        int length = Math.max(lengthInBits, otherLengthInBits);
+        int prefix = maxLengthInBits - length;
+        
+        if (prefix < 0) {
+            return KeyAnalyzer.OUT_OF_BOUNDS_BIT_KEY;
+        }
+        
+        for (int i = 0; i < length; i++) {
+            int index = prefix + (offsetInBits + i);
+            boolean value = isBitSet(key, index, lengthInBits);
+                
+            if (value) {
+                allNull = false;
+            }
+            
+            int otherIndex = prefix + (otherOffsetInBits + i);
+            boolean otherValue = isBitSet(other, otherIndex, otherLengthInBits);
+            
+            if (value != otherValue) {
+                return index;
+            }
+        }
+        
+        if (allNull) {
+            return KeyAnalyzer.NULL_BIT_KEY;
+        }
+        
+        return KeyAnalyzer.EQUAL_BIT_KEY;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isPrefix(BigInteger prefix, int offsetInBits, 
+            int lengthInBits, BigInteger key) {
+        
+        int keyLength = lengthInBits(key);
+        if (lengthInBits > keyLength) {
+            return false;
+        }
+        
+        int elements = lengthInBits - offsetInBits;
+        for (int i = 0; i < elements; i++) {
+            if (isBitSet(prefix, i+offsetInBits, lengthInBits) 
+                    != isBitSet(key, i, keyLength)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
